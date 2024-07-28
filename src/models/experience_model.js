10aeec5ecd6ab9a51fad5e52f5f7e9010c9b368a
wlan07 <albertoalberto58940053@gmail.com>
2024-07-28 23:34:35 +0100

    add experience-section

---
+++ src/models/experience_model.js
@@ -0,0 +1,8 @@
+export default class ExperienceEntry {
+    constructor(title, description, dates,highlighted) {
+        this.title = title;
+        this.description = description;
+        this.dates = dates;
+        this.highlighted = highlighted;
+    }
+}
